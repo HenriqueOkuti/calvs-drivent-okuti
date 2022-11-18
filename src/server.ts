c8541d7e8b76424374e73bce0e62e7192694dc32
Henrique <henrique.krastins@gmail.com>
2022-11-18 06:31:40 -0300

    CHANGE: latest version

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = +process.env.PORT || 4000;
 init().then(() => {
   app.listen(port, () => {
     /* eslint-disable-next-line no-console */
+    console.clear(); //REMOVE!!!
+    /* eslint-disable-next-line no-console */
     console.log(`Server is listening on port ${port}.`);
   });
 });
